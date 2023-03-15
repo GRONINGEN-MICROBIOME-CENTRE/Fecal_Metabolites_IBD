66758d7e66e84e1819ecb068734842a590370fdf
Shixian Hu <32686413+shixianhu@users.noreply.github.com>
2023-03-15 10:28:58 +0800

    Update 16.1_Metabolite_GWAS.sh

--- Scripts/16.1_Metabolite_GWAS.sh
+++ Scripts/16.1_Metabolite_GWAS.sh
@@ -1,6 +1,6 @@
 
 module load Java
-java -XX:ParallelGCThreads=5 -Xmx10G -jar /groups/umcg-gastrocol/tmp01/Metabolic_Project/Settings/eqtl-mapping-pipeline-1.4nZ/eqtl-mapping-pipeline.jar --mode metaqtl --settings Marc_XML.xml
+java -XX:ParallelGCThreads=5 -Xmx10G -jar /groups/umcg-gastrocol/tmp01/Metabolic_Project/Settings/eqtl-mapping-pipeline-1.4nZ/eqtl-mapping-pipeline.jar --mode metaqtl --settings Config_XML.xml
 
 cd Output_chrtmp
 zcat eQTLs.txt.gz | awk '{OFS="\t"}{print $1,$2,$3,$4,$5,$9,$10,$12,$13,$14,$18}' > eQTL.batchtmp.chrtmp.summary.txt
